97c7c36bdb7129aca83e926a69bf7949f7e408e2
naim-ea <toast.nine@gmail.com>
2017-03-28 18:44:54 +0200

    :sparkles: Implementing front-end developement with API requests

--- dist/views/pages/page.php
+++ dist/views/pages/page.php
@@ -18,26 +18,45 @@ curl_close($curl);
 // Json decode
 $result = json_decode($result);
 
+?>
 
+<form action="#" method="get">
+    <input type="text" name="search">
+    <input type="submit" value="VAZY FAIS LA RECHERCHE">
+</form>
+
+
+<?php
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+exit();
+
+if(empty($_GET["search"])){
+    $_GET["search"] = " ";
+}
+$search = $_GET["search"];
 
 for($i = 0; $i < count($result->launches); $i++){
-?> <p><?=$result->launches[$i]->id?></p>
-<p><?=$result->launches[$i]->windowstart?></p>
-<p><?=$result->launches[$i]->windowend?></p>
-<p><?=$result->launches[$i]->name?></p>
-<?php if(!empty($result->launches[$i]->missions)){
-for($j = 0; $j < count($result->launches[$i]->missions); $j++){?>
-<p><?=$result->launches[$i]->missions[0]->name?></p>
-<p><?=$result->launches[$i]->missions[0]->description?></p>
-<?php } }
+    if (stripos($result->launches[$i]->name,$search) > -1 ) {?>
+ <p>---------------------------------------------</p>
+    <p><?=$result->launches[$i]->id?></p>
+    <p><?=$result->launches[$i]->windowstart?></p>
+    <p><?=$result->launches[$i]->windowend?></p>
+    <p><?=$result->launches[$i]->name?></p>
+<?php 
+if(!empty($result->launches[$i]->missions)){
+    for($j = 0; $j < count($result->launches[$i]->missions); $j++){?>
+        <p><?=$result->launches[$i]->missions[0]->name?></p>
+        <p><?=$result->launches[$i]->missions[0]->description?></p>
+    <?php } 
+}
 if(!empty($result->launches[$i]->vidURLs)){?>
-<p><?=$result->launches[$i]->vidURLs[0]?></p>
-<?php }
+    <p><?=$result->launches[$i]->vidURLs[0]?></p>
+<?php } }
 }
 
-//echo '<pre>';
-//print_r($result);
-//echo '</pre>';
-//exit;
+
 
 ?>
--- dist/views/pages/timeline.php
+++ dist/views/pages/timeline.php
@@ -1,120 +1,57 @@
-<div class="container">
-  <header class="header">
-    <a href="#" class="header--logo">
+<?php
+
+$headers = array( 
+    'Accept: application/json'
+);
+
+$ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36';
+
+// Instantiate curl
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://launchlibrary.net/1.2/launch/next/200/');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); 
+curl_setopt($curl, CURLOPT_USERAGENT, $ua);
+$result = curl_exec($curl);
+curl_close($curl);
+
+// Json decode
+$result = json_decode($result);
+
+?>
+
+
+    <div class="container">
+        <header class="header">
+            <a href="#" class="header--logo">
       <img class="header--logo--img" src="<?=URL?>/assets/img/logo.svg" alt="logo">
     </a>
-    <form class="header--search" action="#" method="post">
-      <button class="header--search--btn" type="submit" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
-      <input class="header--search--input" type="text" name="search" value="" placeholder="Search for a mission">
-    </form>
-  </header>
-  <div class="timeline">
-    <div class="item--container">
-      <div class="item">
-      <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-      <div class="item--content">
-        <h2 class="item--content--title">Falcon Full Trusts</h2>
-        <span class="item--content--date">21/03/2017</span>
-        <p class="item--content--txt">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ed do eiusmod tempor incididunt labore.
-        <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-      </div>
-      <div class="item--line"></div>
+            <form class="header--search" action="#" method="post">
+                <button class="header--search--btn" type="submit" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+                <input class="header--search--input" type="text" name="search" value="" placeholder="Search for a mission">
+            </form>
+        </header>
+        <div class="timeline">
+            <?php for($i = 0; $i < count($result->launches); $i++){?>
+            <div class="item--container">
+                <div class="item">
+                    <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
+                    <div class="item--content">
+                        <h2 class="item--content--title"><?=$result->launches[$i]->name?></h2>
+                        <span class="item--content--date"><?=$result->launches[$i]->windowstart?></span>
+                        <?php if(!empty($result->launches[$i]->missions)){?>
+                        <p class="item--content--txt">
+                            <?=$result->launches[$i]->missions[0]->description?></p>
+                            <?php } ?>
+                            <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
+                    </div>
+                    <div class="item--line"></div>
+                </div>
+            </div>
+            <?php } ?>
+            
+        </div>
+        <div class="line"></div>
+        <a class="previous" href="#">Previous</a>
+        <a class="next" href="#">Next</a>
     </div>
-    </div>
-    <div class="item--container">
-      <div class="item">
-      <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-      <div class="item--content">
-        <h2 class="item--content--title">Falcon Full Trusts</h2>
-        <span class="item--content--date">21/03/2017</span>
-        <p class="item--content--txt">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ed do eiusmod tempor incididunt labore.
-        <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-      </div>
-      <div class="item--line"></div>
-    </div>
-    </div>
-    <div class="item--container">
-      <div class="item">
-      <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-      <div class="item--content">
-        <h2 class="item--content--title">Falcon Full Trusts</h2>
-        <span class="item--content--date">21/03/2017</span>
-        <p class="item--content--txt">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ed do eiusmod tempor incididunt labore.
-        <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-      </div>
-      <div class="item--line"></div>
-    </div>
-    </div>
-    <div class="item--container">
-      <div class="item">
-      <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-      <div class="item--content">
-        <h2 class="item--content--title">Falcon Full Trusts</h2>
-        <span class="item--content--date">21/03/2017</span>
-        <p class="item--content--txt">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ed do eiusmod tempor incididunt labore.
-        <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-      </div>
-      <div class="item--line"></div>
-    </div>
-    </div>
-    <div class="item--container">
-      <div class="item">
-      <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-      <div class="item--content">
-        <h2 class="item--content--title">Falcon Full Trusts</h2>
-        <span class="item--content--date">21/03/2017</span>
-        <p class="item--content--txt">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ed do eiusmod tempor incididunt labore.
-        <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-      </div>
-      <div class="item--line"></div>
-    </div>
-    </div>
-    <div class="item--container">
-      <div class="item">
-      <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-      <div class="item--content">
-        <h2 class="item--content--title">Falcon Full Trusts</h2>
-        <span class="item--content--date">21/03/2017</span>
-        <p class="item--content--txt">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ed do eiusmod tempor incididunt labore.
-        <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-      </div>
-      <div class="item--line"></div>
-    </div>
-    </div>
-    <div class="item--container">
-      <div class="item">
-      <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-      <div class="item--content">
-        <h2 class="item--content--title">Falcon Full Trusts</h2>
-        <span class="item--content--date">21/03/2017</span>
-        <p class="item--content--txt">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ed do eiusmod tempor incididunt labore.
-        <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-      </div>
-      <div class="item--line"></div>
-    </div>
-    </div>
-    <div class="item--container">
-      <div class="item">
-      <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-      <div class="item--content">
-        <h2 class="item--content--title">Falcon Full Trusts</h2>
-        <span class="item--content--date">21/03/2017</span>
-        <p class="item--content--txt">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ed do eiusmod tempor incididunt labore.
-        <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-      </div>
-      <div class="item--line"></div>
-    </div>
-    </div>
-  </div>
-  <div class="line"></div>
-  <a class="previous" href="#">Previous</a>
-  <a class="next" href="#">Next</a>
-</div>
