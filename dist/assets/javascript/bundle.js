64a8ca94fbe5b655d5269e63a6f40c511a20470c
timotheehatton <timothee.hatton@gmail.com>
2017-03-29 01:22:38 +0200

    :lipstick: Timeline algo & UI dev & API img

--- dist/assets/javascript/bundle.js
+++ dist/assets/javascript/bundle.js
@@ -7,36 +7,60 @@ var timeline = document.querySelector('.timeline'),
     window_width,
     btn_next = document.querySelector('.next');
 
+//screen cover
 function cover() {
   window_width = window.innerWidth;
   for (var i = 0; i < items.length; i++) {
     items[i].style.width = window_width / 2 + 'px';
-  }
-  timeline.style.width = window_width * items.length + 'px';
+  }timeline.style.width = window_width * items.length + 'px';
   timeline.style.marginLeft = -window_width / 4 + 'px';
 }
 cover();
 window.addEventListener('resize', cover);
 
-function slider() {
-  var i = 0;
-  items[1].classList.add('item--active');
-  btn_next.addEventListener('click', function (e) {
-    i++;
-    if (i == items.length - 1) i = 0;
-    items[i].classList.remove('item--active');
-    items[i + 1].classList.add('item--active');
-    timeline.style.transform = 'translate(-' + window_width / 2 * i + 'px)';
-    e.preventDefault();
-  });
-  btn_previous.addEventListener('click', function (e) {
-    if (i === 0) i = items.length - 1;
-    i--;
-    timeline.style.transform = 'translate(-' + window_width / 2 * i + 'px)';
-    e.preventDefault();
-  });
+//timeline
+var i = 0;
+items[i].classList.add('item--active');
+timeline.style.transform = 'translate(' + window_width / 2 + 'px)';
+function next_slide() {
+  items[i].classList.remove('item--active');
+  i++;
+  if (i == items.length) {
+    i = 0;
+    timeline.style.transform = 'translate(' + window_width / 2 + 'px)';
+  }
+  items[i].classList.add('item--active');
+  timeline.style.transform = 'translate(-' + window_width / 2 * (i - 1) + 'px)';
 }
-slider();
+function previous_slide() {
+  items[i].classList.remove('item--active');
+  i--;
+  if (i < 0) i = items.length - 1;
+  items[i].classList.add('item--active');
+  if (i === 0) timeline.style.transform = 'translate(' + window_width / 2 + 'px)';
+  timeline.style.transform = 'translate(-' + window_width / 2 * (i - 1) + 'px)';
+}
+
+//btn control
+btn_next.addEventListener('click', function (event) {
+  next_slide();
+  event.preventDefault();
+});
+btn_previous.addEventListener('click', function (event) {
+  previous_slide();
+  event.preventDefault();
+});
+
+//key control
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode == 37) {
+    previous_slide();
+    event.preventDefault();
+  } else if (event.keyCode == 39) {
+    next_slide();
+    event.preventDefault();
+  }
+});
 
 },{}]},{},[1])
 
--- dist/views/pages/timeline.php
+++ dist/views/pages/timeline.php
@@ -1,57 +1,56 @@
 <?php
 
-$headers = array( 
-    'Accept: application/json'
-);
+  $headers = array('Accept: application/json');
 
-$ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36';
+  $ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36';
 
-// Instantiate curl
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, 'https://launchlibrary.net/1.2/launch/next/200/');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); 
-curl_setopt($curl, CURLOPT_USERAGENT, $ua);
-$result = curl_exec($curl);
-curl_close($curl);
+  // Instantiate curl
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, 'https://launchlibrary.net/1.2/launch/next/200/');
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($curl, CURLOPT_USERAGENT, $ua);
+  $result = curl_exec($curl);
+  curl_close($curl);
 
-// Json decode
-$result = json_decode($result);
+  // Json decode
+  $result = json_decode($result);
 
 ?>
-
-
-    <div class="container">
-        <header class="header">
-            <a href="#" class="header--logo">
+<div class="container">
+  <header class="header">
+    <a href="#" class="header--logo">
       <img class="header--logo--img" src="<?=URL?>/assets/img/logo.svg" alt="logo">
     </a>
-            <form class="header--search" action="#" method="post">
-                <button class="header--search--btn" type="submit" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
-                <input class="header--search--input" type="text" name="search" value="" placeholder="Search for a mission">
-            </form>
-        </header>
-        <div class="timeline">
-            <?php for($i = 0; $i < count($result->launches); $i++){?>
-            <div class="item--container">
-                <div class="item">
-                    <img class="item--img" src="<?=URL?>/assets/img/rocket.jpg" alt="">
-                    <div class="item--content">
-                        <h2 class="item--content--title"><?=$result->launches[$i]->name?></h2>
-                        <span class="item--content--date"><?=$result->launches[$i]->windowstart?></span>
-                        <?php if(!empty($result->launches[$i]->missions)){?>
-                        <p class="item--content--txt">
-                            <?=$result->launches[$i]->missions[0]->description?></p>
-                            <?php } ?>
-                            <a class="item--content--btn" href="#"><img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission</a>
-                    </div>
-                    <div class="item--line"></div>
-                </div>
+    <form class="header--search" action="#" method="post">
+      <button class="header--search--btn" type="submit" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+      <input class="header--search--input" type="text" name="search" value="" placeholder="Search for a mission">
+    </form>
+  </header>
+    <div class="timeline">
+      <?php for($i = 0; $i < count($result->launches); $i++): ?>
+        <div class="item--container">
+          <div class="item">
+            <img class="item--img" src="<?=$result->launches[$i]->rocket->imageURL?>" alt="">
+            <div class="item--content">
+              <h2 class="item--content--title"><?=$result->launches[$i]->name?></h2>
+              <span class="item--content--date"><?=$result->launches[$i]->windowstart?></span>
+              <?php if(!empty($result->launches[$i]->missions)): ?>
+                <p class="item--content--txt"><?=$result->launches[$i]->missions[0]->description?></p>
+              <?php endif; ?>
+              <a class="item--content--btn" href="#">
+                <img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission
+              </a>
             </div>
-            <?php } ?>
-            
+            <div class="item--line"></div>
+          </div>
+          <div class="item--month">Mars</div>
         </div>
-        <div class="line"></div>
-        <a class="previous" href="#">Previous</a>
-        <a class="next" href="#">Next</a>
+      <?php endfor; ?>
     </div>
+    <a class="previous" href="#">Previous</a>
+    <a class="next" href="#">Next</a>
+    <div class="shadow shadow--left"></div>
+    <div class="shadow shadow--right"></div>
+    <div class="line"></div>
+</div>
