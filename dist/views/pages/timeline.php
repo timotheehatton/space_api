b95dd76f1523e9833c1efb3358855dc6275b98e7
naim-ea <toast.nine@gmail.com>
2017-03-29 11:38:19 +0200

    :spakles: Added the location of launching

--- dist/views/pages/timeline.php
+++ dist/views/pages/timeline.php
@@ -16,41 +16,51 @@
   // Json decode
   $result = json_decode($result);
 
+
 ?>
-<div class="container">
-  <header class="header">
-    <a href="#" class="header--logo">
+    <div class="container">
+        <header class="header">
+            <a href="#" class="header--logo">
       <img class="header--logo--img" src="<?=URL?>/assets/img/logo.svg" alt="logo">
     </a>
-    <form class="header--search" action="#" method="post">
-      <button class="header--search--btn" type="submit" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
-      <input class="header--search--input" type="text" name="search" value="" placeholder="Search for a mission">
-    </form>
-  </header>
-    <div class="timeline">
-      <?php for($i = 0; $i < count($result->launches); $i++): ?>
-        <div class="item--container">
-          <div class="item">
-            <img class="item--img" src="<?=$result->launches[$i]->rocket->imageURL?>" alt="">
-            <div class="item--content">
-              <h2 class="item--content--title"><?=$result->launches[$i]->name?></h2>
-              <span class="item--content--date"><?=$result->launches[$i]->windowstart?></span>
-              <?php if(!empty($result->launches[$i]->missions)): ?>
-                <p class="item--content--txt"><?=$result->launches[$i]->missions[0]->description?></p>
-              <?php endif; ?>
-              <a class="item--content--btn" href="#">
+            <form class="header--search" action="#" method="post">
+                <button class="header--search--btn" type="submit" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+                <input class="header--search--input" type="text" name="search" value="" placeholder="Search for a mission">
+            </form>
+        </header>
+        <div class="timeline">
+            <?php for($i = 0; $i < count($result->launches); $i++): ?>
+            <div class="item--container">
+                <div class="item">
+                    <img class="item--img" src="<?=$result->launches[$i]->rocket->imageURL?>" alt="">
+                    <div class="item--content">
+                        <h2 class="item--content--title">
+                            <?= $result->launches[$i]->name ?>
+                        </h2>
+                        <span class="item--content--date"><?=$result->launches[$i]->windowstart?></span>
+                        <p class="item--content--txt">
+                            <?php if(!empty($result->launches[$i]->missions)){ ?>
+                                <?=$result->launches[$i]->missions[0]->description?>
+                            <?php } ?> }
+                        </p>
+                        <p class="location">
+                            <?=$result->launches[$i]->location->name?>
+                        </p>
+                        <a class="item--content--btn" href="#">
                 <img class="item--content--btn--icon" src="<?=URL?>/assets/img/arrow.png" alt="arrow">Discover the mission
               </a>
+                    </div>
+                    <div class="item--line"></div>
+                </div>
+                <div class="item--month">
+                    <?=date('F', strtotime($result->launches[$i]->windowstart))?>
+                </div>
             </div>
-            <div class="item--line"></div>
-          </div>
-          <div class="item--month"><?=date('F', strtotime($result->launches[$i]->windowstart))?></div>
+            <?php endfor; ?>
         </div>
-      <?php endfor; ?>
+        <a class="previous" href="#">Previous</a>
+        <a class="next" href="#">Next</a>
+        <div class="shadow shadow--left"></div>
+        <div class="shadow shadow--right"></div>
+        <div class="line"></div>
     </div>
-    <a class="previous" href="#">Previous</a>
-    <a class="next" href="#">Next</a>
-    <div class="shadow shadow--left"></div>
-    <div class="shadow shadow--right"></div>
-    <div class="line"></div>
-</div>
