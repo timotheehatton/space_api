d449506219883507be2c06fb31d19041a28f0e53
timotheehatton <timothee.hatton@gmail.com>
2017-03-30 23:05:20 +0200

    :lipstick: Emailing template

--- dist/newsletter.php
+++ dist/newsletter.php
@@ -1,7 +1,7 @@
 <?php
 include 'config.php';
 if(!empty($_POST)){
-    
+
     $add_mail = $_POST["email"];
     $add_mission = $_POST["mission-hidden"];
         $query = $pdo->prepare('INSERT INTO emails(email, mission) VALUES (:email, :mission)');
