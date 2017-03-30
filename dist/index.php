811ee1072faca3dab244ed074deb4734294c9c7a
timotheehatton <timothee.hatton@gmail.com>
2017-03-31 01:03:19 +0200

    :lipstick: Favicons

--- dist/index.php
+++ dist/index.php
@@ -7,6 +7,12 @@ $q = isset($_GET['q']) ? $_GET['q'] : '';
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#10202c">
+    <meta name="theme-color" content="#ffffff">
     <title>Launch News</title>
     <link rel="stylesheet" href="./assets/stylesheet/reset.css">
     <link rel="stylesheet" href="./assets/stylesheet/font_awesome/css/font-awesome.min.css">
